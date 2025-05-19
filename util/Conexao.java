897ac49fe74e9784a8ea76e807465475c6f659a2
Sabrina Gama <62630517+SabrinaGamaa@users.noreply.github.com>
2025-05-19 20:53:23 -0300

    Criando o banco de dados

--- util/Conexao.java
+++ util/Conexao.java
@@ -1,4 +1,20 @@
 package util;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class Conexao {
+    private static final String path = "jdbc:sqlite:database/bancodados.db";
+
+//  Conectar no banco de dados na pasta database
+    public static Connection connection(){
+        try {
+//          Retorna a conexao com banco de dados
+            return DriverManager.getConnection(path);
+        } catch (SQLException e) {
+            System.out.println("Erro na conexão SQL: " + e.getMessage());
+            return null;
+        }
+    }
 }
